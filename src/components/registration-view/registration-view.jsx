b42dc848704a401cdabff1b5d96b8a004b33b19f
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-11-21 19:31:54 -0600

    Added more to registration view

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button, Col, Row } from 'react-bootstrap';
+import { Form, Button, Col, Row, CardGroup, Card, Container } from 'react-bootstrap';
 
 import './registration-view.scss';
 //import axios from 'axios';
